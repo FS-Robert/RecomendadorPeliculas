f0b7869d6a8558996dee7fb797ead911bd0be99c
FS-Robert <robertoantoniolopezramirez1@gmail.com>
2025-10-15 22:22:36 -0600

    modificacion

--- movie.py
+++ movie.py
@@ -31,8 +31,7 @@ ratings = pd.read_csv("ratings.csv", encoding='utf-8')
 movies = movies.head(5000)
 ratings = ratings[ratings['movieId'].isin(movies['movieId'])]
 
-movies = movies.head(5000)
-ratings = ratings[ratings['movieId'].isin(movies['movieId'])]
+
 
 # Limpiar títulos
 movies['clean_title'] = movies['title'].str.replace(r'\(\d{4}\)', '', regex=True).str.strip()
